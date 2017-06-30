4945867611578f32a0e44521426a20a935a69fc4
USER <USER@example.com>
2017-06-30 10:52:20 +0900

    추가

---
+++ Calculator2/src/Calculator2.java
@@ -0,0 +1,16 @@
+
+public class Calculator2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//주석추가
+		
+	}
+	
+	public int add(int a, int b){
+		int sum = 0;
+		
+		return sum;
+	}
+
+}
